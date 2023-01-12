06c019e6f7fbed5c8d8685cacccdbec7eca935a4
JoaoRafaelLima <jrafael0618@gmail.com>
2023-01-12 09:02:14 -0300

    Usando o shortcut get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipe.factory import make_recipe
 from .models import Recipe
-from django.http import Http404
+
 
 # Create your views here.
 def home(request):
@@ -11,16 +11,23 @@ def home(request):
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True
-        ).order_by("-id")
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id,
+    #     is_published=True
+    #     ).order_by("-id")
 
-    if not recipes:
-        raise  Http404("Not Found")
+    # if not recipes:
+    #     raise  Http404("Not Found")
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+         category__id=category_id,
+         is_published=True
+         ).order_by("-id")
+    )
+
     return render(request, "recipes/pages/category.html", context={
         "recipes": recipes,
-        "title": f"{recipes.first().category.name } - Category |"
+        "title": f"{recipes[0].category.name } - Category |"
     })
 
 def recipe(request, id):
