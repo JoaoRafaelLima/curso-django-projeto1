28e8713efe9e20ce7ceb62a02d5cbf4a3971d9c7
JoaoRafaelLima <jrafael0618@gmail.com>
2023-02-14 21:53:30 -0300

    Utilizando clean_field

--- authors/forms.py
+++ authors/forms.py
@@ -1,13 +1,14 @@
 from django import forms 
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ValidationError
 
 def add_attr(field, attr_name, attr_new_val):
     existing_attr = field.widget.attrs.get(attr_name, '')
     field.widget.attrs[attr_name] = f"{existing_attr} {attr_new_val}".strip()
 
 def add_placeholder(field, placeholder_val):
-    field.widget.attrs["placeholder"] = placeholder_val
+    add_attr(field, "placeholder", placeholder_val)
+   
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -63,3 +64,12 @@ class RegisterForm(forms.ModelForm):
             "placeholder": "Type your password here"
             })
         }
+    def clean_password(self):
+        data = self.cleaned_data.get("password")
+        if "atenção" in data:
+            raise ValidationError(
+                'Não digite %(value)s  no campo password',
+                code='invalid',
+                params={'value': "atenção"}
+            )
+        return data
