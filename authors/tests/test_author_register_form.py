df4ea430aa62c44d336bfa653dcd31ce262f41f0
JoaoRafaelLima <jrafael0618@gmail.com>
2023-02-16 14:16:53 -0300

    Testando o help_text

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -12,7 +12,25 @@ class AuthorRegisterFormUnitTest(TestCase):
         ("password", "Type your password here"),
         ("password2", "Repeat your password here"),
     ])
-    def test_first_name_placeholder_is_correct(self, field, placeholder):
+    def test_fields_placeholder(self, field, placeholder):
+        #test_first_name_placeholder_is_correct
         form = RegisterForm()
         current_placeholder = form[field].field.widget.attrs["placeholder"]
         self.assertEqual(placeholder, current_placeholder)
+
+    @parameterized.expand([
+        ("password",
+            (
+                'Password must have at least one uppercase letter, '
+                'one lowercase letter and one number. The length should be '
+                'at least 8 characters.'
+            ),
+        ),
+        ("email", "The e-mail must be a valid"),
+        ("username", "Obrigatório. 150 caracteres ou menos. Letras, números e @/./+/-/_ apenas.")
+    ])
+    def test_fields_help_text(self, field, needed):
+        form = RegisterForm()
+        current = form[field].field.help_text
+
+        self.assertEqual(current, needed)
