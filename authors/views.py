9092fcbe7d57a9baf3a6aacec22290dcbd89fe38
JoaoRafaelLima <jrafael0618@gmail.com>
2023-02-19 11:51:05 -0300

    Reorganizando os arquivos da aplicação

--- authors/views.py
+++ authors/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.http import Http404
 from django.contrib import messages
+from django.urls import reverse
+
 
 # Create your views here.
 def register_view(request):
@@ -9,7 +11,8 @@ def register_view(request):
     regiter_form_data = request.session.get("regiter_form_data", None)
     form = RegisterForm(regiter_form_data)
     return render(request, "authors/pages/register_view.html", context={
-        "form": form
+        "form": form,
+        "form_action": reverse('authors:create')
     })
 def register_create(request):
     if not request.POST:
