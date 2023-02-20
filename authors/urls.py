1248802a203f5e48f0715354cb6ba7599035029c
JoaoRafaelLima <jrafael0618@gmail.com>
2023-02-20 10:33:19 -0300

    Criando o formulário de login

--- authors/urls.py
+++ authors/urls.py
@@ -6,6 +6,6 @@ app_name = "authors"
 urlpatterns = [
     path("register/", register_view, name="register"),
     path("register/create/", register_create, name="register_create"),
-    path("login/", register_view, name="login"),
-    path("login/create/", register_create, name="login_create"),
+    path("login/", login_view, name="login"),
+    path("login/create/", login_create, name="login_create"),
 ]
