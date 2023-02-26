11ff261c72f7b539893045185f30492ca185616a
JoaoRafaelLima <jrafael0618@gmail.com>
2023-02-26 11:03:49 -0300

    Organizando os testes funcionais

--- tests/functional_tests/recipes/base.py
+++ tests/functional_tests/recipes/base.py
@@ -1,10 +1,9 @@
+import time
 # inicia um servidor de test sem arquivos staticos
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
 # inicia um servidor de test com arquivos staticos
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from utils.browser import make_chrome_browser
-from selenium.webdriver.common.by import By
-import time
 
 class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
     def setUp(self) -> None:
@@ -17,9 +16,3 @@ class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
     
     def sleep(self, seconds=5):
         time.sleep(seconds)
-class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
-    def test_recipe_home_page_without_recipes_not_found_message(self):
-        self.browser.get(self.live_server_url)
-        body = self.browser.find_element(By.TAG_NAME, "body")
-        self.assertIn("No recipes found here.", body.text)
-        
